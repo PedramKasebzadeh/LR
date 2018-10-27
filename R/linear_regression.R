3e82bda4028495e5d9ced2f660be43f068c1c9fd
RoshniSundaramurthy09 <43231073+RoshniSundaramurthy09@users.noreply.github.com>
2018-10-27 16:24:52 +0200

    updated doc

--- R/linear_regression.R
+++ R/linear_regression.R
@@ -1,10 +1,19 @@
-#' A Reference Class for cumputating Linear Regression
+#' A Reference Class for computing Linear Regression using OLS
 #'
-#'This class has a vary of functions.
-#'@param formula A formula
-#'@param data A data frame 
-#' @field computing a liner regression 
-#' library(ggplot2)
+#' This class has a varied methods.
+#' @param formula A formula
+#' @param data A data frame 
+
+#' @field computing linear regression
+
+#' @return print() prints out the coefficients and coefficient names,
+#'         plot() plots the following two plots using ggplot2,
+#'         resid() returns the vector of residuals e,
+#'         pred() returns the predicted values of ybar,
+#'         coef() returns the coefficients as a named vector,
+#'         summary() returns a similar printout as printed for lm objects.
+
+#' @import methods
 #' @export
 
 #0.5  0.1  0.0
@@ -12,11 +21,11 @@
 linreg <- setRefClass(Class = "linreg",
                       
                     
-                      fields = list(formula="formula", data="data.frame", regco="matrix", yf="matrix", e="matrix", dfreedom="numeric", 
-                                    Sigma_square="numeric", Var_Beta="matrix", t_Beta="matrix", pvalue="matrix",
-                                    parse="character", stand_res="matrix",variance="numeric"),
-                      
-                      
+                      fields = list(formula="formula", data="data.frame", regco="matrix",
+                                    yf="matrix", e="matrix", dfreedom="numeric", 
+                                    Sigma_square="numeric", Var_Beta="matrix", t_Beta="matrix", 
+                                    pvalue="matrix",parse="character", stand_res="matrix",
+                                    variance="numeric"),
                       
                       methods = list(
                         
@@ -30,7 +39,7 @@ linreg <- setRefClass(Class = "linreg",
                           data <<- data
                           X <- model.matrix(formula,data)
                           dep_y <- all.vars(formula)[1]
-                          y <- (data[,dep_y])
+                          y <- as.matrix(data[dep_y]) 
                           parse <<- deparse(substitute(data))
                           #Regressions coefficients
                           regco <<- solve((t(X)%*%X))%*%t(X)%*%y
@@ -56,23 +65,26 @@ linreg <- setRefClass(Class = "linreg",
                           stand_res <<- sqrt(abs((e-mean(e)) / sqrt(Sigma_square)))
                         
                         },
-                        #Methods
-                        #print(),plot(), resid(),pred(),coef(),summary()
-                        print = function(){
-                          cat("Coefficients:","\n",
-                              paste(row.names(regco)),"\n",
-                              format(round(regco,2),width = 12)
-                           )   
-                           },
-                            
                         
+                        #print coefficients and coefficient names
+                        print = function(){
+                          cat("\n","Call:","\n",
+                          paste0("linreg(formula = ", format(formula), ", data = ", parse , ")\n\n ", sep = ""))
+                          cat("\n","Coefficients:","\n")
+                          (setNames(round(regco[1:nrow(regco)],2),rownames(regco)))
+                          
+                          },
+                       
+                        #plot()
                         plot = function(){
                           library(ggplot2)
                           library(ggThemeAssist)
-                          
-                            LiU_theme <-  theme(
-                            axis.title.x = element_text(color = "#38ccd6", size = 14, face = "bold"),
-                            axis.title.y = element_text(color = "#38ccd6", size = 14, face = "bold"),
+                          # Liu theme
+                          LiU_theme <-  theme(
+                            axis.title.x = element_text(color = "#38ccd6", size = 14,
+                                                        face = "bold"),
+                            axis.title.y = element_text(color = "#38ccd6", size = 14,
+                                                        face = "bold"),
                             axis.text = element_text(color = "#1c1c19", size = 6),
                             axis.line = element_line(color = "#1c1c19", size = 0.5),
                             axis.ticks = element_line(color = "#38ccd6", size = 0.5),
@@ -80,23 +92,26 @@ linreg <- setRefClass(Class = "linreg",
                             axis.text.y = element_text(size = 8),
                             panel.background = element_rect(fill = "white", color = NA),
                             panel.grid.major = element_line(color = "#1c1c19", size = 0.5),
-                            panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(), 
-                            panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(),
+                            panel.grid.major.x = element_blank(),
+                            panel.grid.minor.x = element_blank(), 
+                            panel.grid.major.y = element_blank(),
+                            panel.grid.minor.y = element_blank(),
                             panel.grid.minor = element_line(color = "#1c1c19", size = 5),
                             plot.background = element_rect(color = "black"),
-                            plot.title = element_text(color = "#38ccd6", size = 20, face = "bold",hjust = 0.5),
+                            plot.title = element_text(color = "#38ccd6", size = 20,
+                                                      face = "bold",hjust = 0.5),
                             plot.caption = element_text(size = 10,hjust=0.5),
                             plot.margin = unit(c(1.2,1.2,1.2,1.2), "cm")
                             )
                           
-                                
-                          
-                          title <- paste("Fitted values linreg(", formula[2]," ", formula[1], " ", formula[3], ")")
+                          title <- paste("Fitted values linreg(", formula[2]," ", formula[1], " ",
+                                         formula[3], ")")
                               
                           #plotting yf and e
                           data_frame1 <- data.frame(Fitted_values=yf,Residuals=e)
                           p1 <- ggplot(data_frame1,aes(Fitted_values,Residuals))+
-                            geom_point(shape = 21, colour = "black", fill = "white", size = 2.8, stroke = 1.3)+
+                            geom_point(shape = 21, colour = "black", fill = "white", size = 2.8,
+                                       stroke = 1.3)+
                             geom_smooth(method = "loess",color = "red", se = FALSE)+
                             ggtitle("Residuals vs Fitted")+
                             xlab(title)+
@@ -108,7 +123,8 @@ linreg <- setRefClass(Class = "linreg",
                           
                           data_frame2 <- data.frame(Fitted_values=yf,Residuals=stand_res)
                           p2 <- ggplot(data_frame2,aes(Fitted_values,Residuals))+
-                            geom_point(shape = 21, colour = "black", fill = "white", size = 2.8, stroke = 1.3)+
+                            geom_point(shape = 21, colour = "black", fill = "white", size = 2.8,
+                                       stroke = 1.3)+
                             geom_smooth(method = "loess",color = "red", se = FALSE)+
                             ggtitle("Scale-Location")+
                             xlab(title)+
@@ -120,29 +136,42 @@ linreg <- setRefClass(Class = "linreg",
                           return(list(p1,p2))
                         },
                         
+                        #vector of residuals e
                         resid = function(){
                           cat("Returning vector of residuals e:", "\n")
                           return(as.vector(round(e,2)))
                         },
                         
+                        #predicted values y_hat
                         pred = function(){
                           cat("Returning predicted values yf:", "\n")
                           return(as.vector(round(yf,2)))
                         },
                         
+                        #coefficients as a named vector
                         coef = function(){
                           cat("Returning coefficients as a vector:", "\n")
                           return(as.vector(round(regco,2)))
                         },
                         
+                        #summary()
                         summary = function(){
                           
-                          a<-paste("Residual standard error",sqrt(Sigma_square),"on",dfreedom, "degrees of freedom")
-                          
-                          return(list(regco=regco,pvalue=pvalue,t_beta=t_Beta,var_and_degree_f=a))
-                          
+                          cat("linreg(formula = ", format(formula), ", data = ", parse, ") :\n\n ", sep = "")
+                          x <- setNames(as.data.frame(cbind(regco,as.matrix(sqrt(diag(Var_Beta))),t_Beta, formatC(pvalue, format = "e", digits = 2), p_cal(pvalue))), c("Coefficients","Standard error","t-values", "p-values", ""))
+                          print_custom(x)
+                          cat("\n\n Residual standard error: ", sqrt(Sigma_square), " on ", dfreedom, " degrees of freedom ", sep = "")
                         }
-                        
-                        
-                        
+  
                       ))
+print_custom <- function(x){
+  print(x)
+}
+
+p_cal = function(p_val) {
+  x <- ifelse(p_val > 0.1, " ",
+              (ifelse(p_val > 0.05, " . ",
+                      (ifelse(p_val > 0.01, "*",
+                              (ifelse(p_val > 0.001, "**","***")))))))
+  return(x)
+}
